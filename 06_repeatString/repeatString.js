32c8c343e7cc242f0194f5eda4cc2c3da4bc8ac6
Hakan <hakancopuroglu@gmail.com>
2025-09-04 20:34:58 +0200

    complete 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, amount) {
 
+    if(amount < 0) {
+        return "ERROR";
+    }
+
+    const start = str;
+    str = "";
+    for (let index = 0; index < amount; index++) {
+        str+=start;
+    }
+    return str;
 };
 
 // Do not edit below this line
