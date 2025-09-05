e44298b37cc9513a4d35623c44ea82db7113c588
Hakan <hakancopuroglu@gmail.com>
2025-09-05 15:08:54 +0200

    complete 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    for (let index = 0; index < remove.length; index++) {
+    array = array.filter( item => item !== remove[index])   
+    }
+
+    return array;
 };
 
 // Do not edit below this line
