159d078d955ef3b79bd9f6c2469889b23b95a20a
Hakan <hakancopuroglu@gmail.com>
2025-09-05 15:18:56 +0200

    complete 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
 
+    let counter = 0;
+    for (let index = (end - start > 0 ? start : end); index <= (end - start > 0 ? end : start); index++) {
+        counter += index;
+    }
+
+    return counter;
 };
 
 // Do not edit below this line
