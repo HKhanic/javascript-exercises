d680f6d26f6a937d9d208a6d873cc80545622cb5
Hakan <hakancopuroglu@gmail.com>
2025-09-05 14:53:25 +0200

    complete 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const charArray = str.split("");
+    str = "";
+    for (let index = charArray.length - 1; index >= 0; index--) {
+        str += charArray[index];
+    }
+    return str;
 };
 
 // Do not edit below this line
